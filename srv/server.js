44e8f216c3bb385716ad65cb92a29bc4d1a3ff30
Omkar Parhad <omkarparhad88pro@gmail.com>
2025-03-24 05:27:41 +0000

    base end point error and  metadata

--- srv/server.js
+++ srv/server.js
@@ -1,14 +1,17 @@
-require("dotenv").config({ path: "default-env.json" }); 
+require("dotenv").config({ path: "default-env.json" });
 const express = require("express");
 const SapCfAxios = require("sap-cf-axios").default;
 const axios = require("axios");
+var xml = require('xml');
+
+
 
 const app = express();
 const PORT = 8080;
 
 const AxiosInstance = (destinationName) => {
     if (process.env.VCAP_SERVICES) {
-        return SapCfAxios(destinationName); 
+        return SapCfAxios(destinationName);
     } else {
         const destinations = require("./default-env.json").VCAP_SERVICES.destination;
         const destination = destinations.find(dest => dest.name === destinationName);
@@ -19,16 +22,16 @@ const AxiosInstance = (destinationName) => {
             auth: {
                 username: destination.credentials.username,
                 password: destination.credentials.password
-            },
-            headers: { accept: "application/json" }
+            }
         });
     }
 };
 
 
-const axios2 = SapCfAxios("SFSF");
+// const axios2 = SapCfAxios("SFSF");
 const axios1 = AxiosInstance("SFSFDEST");
-const prdComponent = async (req, res) => {
+
+app.get("/prdcomponent", async (req, res) => {
     try {
         const response = await axios1({
             method: "GET",
@@ -36,41 +39,41 @@ const prdComponent = async (req, res) => {
             params: { $format: "json" },
             headers: { accept: "application/json" }
         });
-        res.json(response.data.d.results);
+
+        
+        res.status(200).json(response.data.d.results);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-};
+});
 
-app.get("/prdcomponent", prdComponent);
-
-const metadata = async (req, res) => {
+app.get("/metadata", async (req, res) => {
     try {
         const response = await axios1({
             method: "GET",
-            url: "/odata/v2/$metadata"
+            url: "/odata/v2/$metadata",
+            responseType: "text",
         });
-        res.json(response.data.d.results);
+        res.setHeader("Content-Type", "application/xml"); 
+        res.status(200).send(response.data);
+        // response.pipe(res); // Stream the response directly
     } catch (error) {
+        console.log(error)
         res.status(500).json({ error: error.message });
     }
-};
+});
 
-app.get("/metadata", metadata);
-
-const  base = async (req, res) => {
+app.get("/", async (req, res) => {
     try {
         const response = await axios1({
             method: "GET",
             url: "/odata/v2/"
         });
-        res.json(response.data.d.results);
+        res.status(200).json(response.data);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-};
-
-app.get("/", base);
+});
 
 // const axios2 = SapCfAxios("SF");
 // const qasComponent = async (req, res) => {
