73a7846be60cb489b2c338e6e6b249013cefa26c
OmkarParhad88 <omkarparhad7905@gmail.com>
2025-05-15 16:15:04 +0530

    prestige cicd3

--- srv/utils/utils.js
+++ srv/utils/utils.js
@@ -166,7 +166,7 @@ async function getCtcLetterJsonData(userid) {
   const name = bindSalutationAndName(User?.salutation, User?.displayName);
 
   let response = {
-    version: "1.5",
+    version: "1.6",
     generated_on: getFormattedTimestamp(),
     today: getCurrentFormattedDate(),
     joiningDate: formattedDate,
