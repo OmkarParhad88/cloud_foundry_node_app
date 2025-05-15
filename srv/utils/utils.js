59b90adb9314a2bc27664a8a59f7663836c3aacd
OmkarParhad88 <omkarparhad7905@gmail.com>
2025-05-15 15:22:09 +0530

    test prestige git hub add

--- srv/utils/utils.js
+++ srv/utils/utils.js
@@ -166,7 +166,7 @@ async function getCtcLetterJsonData(userid) {
   const name = bindSalutationAndName(User?.salutation, User?.displayName);
 
   let response = {
-    version: "1.2",
+    version: "1.3",
     generated_on: getFormattedTimestamp(),
     today: getCurrentFormattedDate(),
     joiningDate: formattedDate,
